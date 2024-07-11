05fceb24f209c1a90602c7dabcebd5cd66f4fa21
Georgi Kuklev <gkuklev98@gmail.com>
2024-07-12 01:27:08 +0300

    Compute free variables of a lambda expression.

--- Sources/Lambda/Lambda.swift
+++ Sources/Lambda/Lambda.swift
@@ -4,8 +4,36 @@
 
 class Lambda {
     let lambda: LambdaExpression
+    private(set) lazy var freeVariables: Set<String> = {
+        var all: Set<String> = []
+        var bound: Set<String> = []
+        getVariables(lambda: lambda, all: &all, bound: &bound)
+        return all.subtracting(bound)
+    }()
 
     init(_ lambda: LambdaExpression) {
         self.lambda = lambda
     }
 }
+
+// MARK: Free variables
+extension Lambda {
+    private func getVariables(
+        lambda: LambdaExpression,
+        all: inout Set<String>,
+        bound: inout Set<String>
+    ) {
+        switch lambda {
+        case let .variable(name):
+            all.insert(name)
+        case let .abstraction(variable, body):
+            all.insert(variable)
+            bound.insert(variable)
+            getVariables(lambda: body, all: &all, bound: &bound)
+        case let .application(function, argument):
+            getVariables(lambda: function, all: &all, bound: &bound)
+            getVariables(lambda: argument, all: &all, bound: &bound)
+        }
+    }
+}
+
