90e8c179feedb399474b738e64e741e7f3ff0c00
Georgi Kuklev <georgi.kuklev@mobisystems.com>
2024-07-12 20:08:34 +0300

    Remove unnecessary method.

--- Sources/Lambda/Lambda.swift
+++ Sources/Lambda/Lambda.swift
@@ -18,24 +18,6 @@ class Lambda {
 
 // MARK: Free variables
 extension Lambda {
-    private func getVariables(
-        lambda: LambdaExpression,
-        all: inout Set<String>,
-        bound: inout Set<String>
-    ) {
-        switch lambda {
-        case let .variable(name):
-            all.insert(name)
-        case let .abstraction(variable, body):
-            all.insert(variable)
-            bound.insert(variable)
-            getVariables(lambda: body, all: &all, bound: &bound)
-        case let .application(function, argument):
-            getVariables(lambda: function, all: &all, bound: &bound)
-            getVariables(lambda: argument, all: &all, bound: &bound)
-        }
-    }
-
     private func getFreeVariables(
         lambda: LambdaExpression,
         free: Set<String>,
